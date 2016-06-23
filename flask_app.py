4ed0a8d9598cf43a1949aeb00f7a2c8f64d8d17c
easf <esanchiz92@gmail.com>
2016-06-23 08:48:17 +0000

    set all home path

--- flask_app.py
+++ flask_app.py
@@ -8,30 +8,32 @@ FB_APP_NAME = 'Matching Finder'
 FB_APP_SECRET = 'a6b94f66589e9f9a7d558cda0e83a3dd'
 API_VERSION = 'v2.6'
 
+HOME='/friends'
+
 # instance_path: Please keep in mind that this path must be absolute when provided. , instance_path='/home/easf/friends/instance'
 app = Flask(__name__)
 
 
 #app.config['APPLICATION_ROOT'] = '/home/easf/friends/'
 
-@app.route('/login', methods=['GET','POST'])
+@app.route(HOME + '/login', methods=['GET','POST'])
 def login():
     #names = ['Emanuel', 'Antonio', 'Sanchiz', 'Flores']
     return render_template('login.html', app_id=FB_APP_ID, version=API_VERSION)
 
-@app.route('/data')
+@app.route(HOME + '/data')
 def data():
     token = request.args.get('token', 0, type=str)
     uid = request.args.get('uid', 0, type=str)
     procedures.proof(uid, token)
     return jsonify(result = token)
 
-@app.route('/')
+@app.route(HOME + '/')
 def index():
     return render_template('index.html', app_id=FB_APP_ID, version=API_VERSION)
 
 
-@app.route('/logout')
+@app.route(HOME + '/logout')
 def logout():
     return 'logouuuut'
 
