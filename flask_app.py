a87157670e5e8ff41cf5af36679876753666b10b
easf <esanchiz92@gmail.com>
2016-07-01 11:36:01 +0200

    add config file and set uwsgi

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,9 @@
+import config
+import procedures
 from flask import Flask, url_for, render_template, jsonify, request, redirect
 from flaskext.mysql import MySQL
-import procedures
+from PrefixMiddleware import PrefixMiddleware
+
 
 
 # Facebook app details
@@ -9,41 +12,45 @@ FB_APP_NAME = 'Matching Finder'
 FB_APP_SECRET = 'a6b94f66589e9f9a7d558cda0e83a3dd'
 API_VERSION = 'v2.6'
 
-HOME=''
+# instance_path: Please keep in mind that this path must be absolute when provided. , instance_path='/var/www/friends/instance'
+app = Flask(__name__, instance_path = config.INSTANCE_PATH)
+#application = app  # make uwsgi happy
 
-# instance_path: Please keep in mind that this path must be absolute when provided. , instance_path='/home/easf/friends/instance'
-app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'emanuel'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'friendsdb'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# DATABASE SETTINGS
+
+app.config['MYSQL_DATABASE_USER'] = config.MYSQL_DATABASE_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = config.MYSQL_DATABASE_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = config.MYSQL_DATABASE_DB
+app.config['MYSQL_DATABASE_HOST'] = config.MYSQL_DATABASE_HOST
+
+# PREFIX SETTING
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix = config.PREFIX)
 
 mysql.init_app(app)
 
-#app.config['APPLICATION_ROOT'] = '/home/emanuel/'
 
-@app.route(HOME + '/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     #names = ['Emanuel', 'Antonio', 'Sanchiz', 'Flores']
     return render_template('login.html', app_id=FB_APP_ID, version=API_VERSION)
 
-@app.route(HOME + '/data')
+@app.route('/data')
 def data():
     token = request.args.get('token', 0, type=str)
     uid = request.args.get('uid', 0, type=str)
     procedures.proof(uid, token, mysql)
     return jsonify(result = token)
 
-@app.route(HOME + '/')
+@app.route('/')
 def index():
     return render_template('index.html', app_id=FB_APP_ID, version=API_VERSION)
 
 
-@app.route(HOME + '/logout')
+@app.route('/logout')
 def logout():
     return 'logouuuut'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=False)
